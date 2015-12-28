e60b25536fc2f9f65b96724f3e4e2d9af1c0ef3e
Chris McKnight <cmckni3@gmail.com>
2015-12-28 10:00:45 -0600

    Filters out staging

--- non-master-branches.js
+++ non-master-branches.js
@@ -9,7 +9,7 @@ gitlab.projects.all(function(projects) {
       debug(project.name_with_namespace, project.id);
       debug(result);
       if (result) {
-        var non_master_branches = result.filter(function(branch) { return branch.name !== 'master' && branch.name !== 'quality-assurance'; }).map(function(branch) { return branch; });
+        var non_master_branches = result.filter(function(branch) { return branch.name !== 'master' && branch.name !== 'staging' && branch.name !== 'quality-assurance'; }).map(function(branch) { return branch; });
         if (non_master_branches.length !== 0)
         {
           console.log(project.name_with_namespace, ':', non_master_branches.map(function(branch) { return colors.red.underline(branch.name); }).join(', '));
