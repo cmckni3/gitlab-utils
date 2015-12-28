dabcd63710b4902dd74e7508d3953bf8eda882d0
Chris McKnight <cmckni3@gmail.com>
2015-12-28 09:31:27 -0600

    * Shows project with namespace * Adds debug

--- non-master-branches.js
+++ non-master-branches.js
@@ -1,19 +1,22 @@
 var gitlab = require(process.cwd() + '/gitlab');
 var colors = require('colors');
+var debug  = require('debug')('gitlab-utils:branch-checker');
 
 gitlab.projects.all(function(projects) {
-  var projects = projects.map(function(project) {
-    return {id: project.id, name: project.name_with_namespace};
-  });
   projects.forEach(function(project) {
-    gitlab.projects.repository.listBranches(project.id, function(result)
-    {
-      var non_master_branches = result.filter(function(branch) { return branch.name !== 'master'; }).map(function(branch) { return branch; });
-      if (non_master_branches.length !== 0)
-      {
-        console.log(project.name + ': ' + non_master_branches.map(function(branch) { return colors.red.underline(branch.name); }).join(', '));
+    var name = project.name_with_namespace;
+    gitlab.projects.repository.listBranches(project.id, function(result) {
+      debug(project.name_with_namespace, project.id);
+      debug(result);
+      if (result) {
+        var non_master_branches = result.filter(function(branch) { return branch.name !== 'master' && branch.name !== 'quality-assurance'; }).map(function(branch) { return branch; });
+        if (non_master_branches.length !== 0)
+        {
+          console.log(project.name_with_namespace, ':', non_master_branches.map(function(branch) { return colors.red.underline(branch.name); }).join(', '));
+        }
+      } else {
+        console.log(project.name_with_namespace, ':', colors.yellow.bold('Empty result returned'));
       }
     });
   });
 });
-
--- projects-without-readme.js
+++ projects-without-readme.js
@@ -11,19 +11,16 @@ gitlab.projects.all(function(projects) {
       file_path: 'README.md',
       ref: 'master'
     };
-    gitlab.projects.repository.listTree(project.id, {}, function(result){
-      if (result)
-      {
+    gitlab.projects.repository.listTree(project.id, {}, function(result) {
+      if (result) {
         readme_files = result.filter(function(object) { return object.type === 'blob'; }).filter(function(object) { return /readme/.test(object.name.toLowerCase()); });
         console.log(readme_files);
       }
     });
-    gitlab.projects.repository.showFile(opts, function(result){
-      if (!result)
-      {
+    gitlab.projects.repository.showFile(opts, function(result) {
+      if (!result) {
         console.log('No readme for ' + project.name);
       }
     });
   });
 });
-
